4961a28fad212b1263a1850d025ec4650afd1c4b
Bhaskar Anand <119348690+bhaskar-nie@users.noreply.github.com>
2025-04-15 08:32:19 +0530

    Delete install-dependencies.bat

--- install-dependencies.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing dependencies for Neural Network Visualizer...
-
-cd frontend
-call npm install --save-dev @types/three
-
-echo.
-echo Dependencies installed successfully!
-echo. 
